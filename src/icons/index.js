a7873a9a9f66294bdd8135159eaf3600f95dd85c
mrgao125 <312595543@qq.com>
2022-06-22 11:38:12 +0800

    人力资源项目初始化

---
+++ src/icons/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon' // svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+
+// 将所有svg目录下，带.svg后缀的文件，引入项目中，在  全局注册的组件 svg-icon 中使用
